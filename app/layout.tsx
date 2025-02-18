0c521808329e43ef2e739ba1efd1a1616c43f4df
Bruno Martins <brunobossomartins1@gmail.com>
2025-02-18 09:27:26 -0300

    adicionado favicon do site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
+      <head>
+      <link rel="icon" href="/favicon.png" type="image/x-icon" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
