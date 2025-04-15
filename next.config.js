e6d5101bf4d591e82800b3b1edf7fbadd4d42988
bruno bosso martins <bruno.martins@conttrolare.com.br>
2025-04-15 16:57:36 -0300

    agora vai

--- next.config.js
+++ next.config.js
@@ -1,29 +1,10 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 const nextConfig = {
   // output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
-  experimental: {
-    optimizeCss: true,
-  },
-  // Optimize JavaScript
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  // Optimize loading performance
-  reactStrictMode: true,
-  poweredByHeader: false,
+  images: { unoptimized: true },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
